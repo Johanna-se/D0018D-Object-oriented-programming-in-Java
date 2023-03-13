516b2ab18e59de34ccf561c0d85233f7d9bdaaf3
Johanna Petersson <jojanna.petersson@hotmail.com>
2023-03-13 16:47:03 +0100

    Update Fraga.java

--- Övningstentor/FrageSport/src/ojoepe5/Fraga.java
+++ Övningstentor/FrageSport/src/ojoepe5/Fraga.java
@@ -5,8 +5,14 @@
 
 package ojoepe5;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Fraga 
+public class Fraga implements Serializable
 {
 	//Variabler
     private String fraga;
@@ -20,6 +26,54 @@ public class Fraga
     }
     
     //--------------------------------Metoder för IO--------------------------------------------
+    /**
+    * Metod för att kunna läsa objected från en stream. behövs för att implementera Serializable 
+    * @param ObjectOutputStream streamUt
+    * @return void
+    */
+    private void readObject(ObjectInputStream streamIn) throws ClassNotFoundException, IOException
+    {
+        try
+        {
+        	fraga = streamIn.readUTF();
+        	svar = streamIn.readUTF();
+        }
+        catch (EOFException exc)
+        {
+            //Slut på inläsningen, 
+        }
+        catch (IOException e)
+        {
+            throw new IOException();
+        }
+     }
+     
+     /**
+     * Metod för att kunna läsa objected till en stream. behövs för att implementera Serializable 
+     * @param ObjectOutputStream streamUt
+     * @return void
+     */
+     private void writeObject(ObjectOutputStream streamUt) throws IOException
+     {
+         try
+         {
+             streamUt.writeUTF(fraga);
+             streamUt.writeUTF(svar);
+         }
+         catch (IOException e)
+         {
+             throw new IOException();
+         }
+     }
     
-    //-------------------------------Publika metoder----------------------------------- 
+     //-------------------------------Get metoder-----------------------------------
+     public String getFraga()
+     {
+    	 return fraga;
+     }
+    
+     public String getSvar()
+     {
+    	 return svar;
+     }
 }
