7b9ab074126af7e7ef82d340481007984e2e25dc
Johanna Petersson <jojanna.petersson@hotmail.com>
2023-03-13 09:59:09 +0100

    Create FilhanteringMedGUI.java

---
+++ Övningstentor/Crama AB/src/ojoepe5/FilhanteringMedGUI.java
@@ -0,0 +1,13 @@
+/**
+ * Klass för att skapa en filhanterare med eget GUI
+ * 
+ * @author Johanna Petersson, ojoepe-5
+ */
+
+package ojoepe5;
+import java.io.*;
+
+
+public class FilhanteringMedGUI {
+
+}
